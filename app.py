7c1f2fb9244324073645d99be798309fa95ade1f
AlexMalini <alexanderdvm1971@gmail.com>
2026-07-17 11:31:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ app = Flask(__name__)
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = "SmartCampus.event2026@gmail.com"
-app.config["MAIL_PASSWORD"] = "zobq iyng jkgb ujdh"
+app.config["MAIL_USERNAME"] = "your_email@gmail.com"
+app.config["MAIL_PASSWORD"] = "your_app_password"
 
 mail = Mail(app)
 
